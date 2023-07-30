ca4bed8c58b7a0b0329bd65f860a8059dd8ee48e
Sharifa Abdirizak <sharifamabdirizak@gmail.com>
2023-07-30 15:09:20 -0500

    updated to part-supply.js

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -3,20 +3,24 @@ console.log('****** Part Supply *******');
 
 // 1. Create a variable called 'partsNeeded' with a value of the number 40 
 //    & console.log the variable
+let partsNeeded = 40
 console.log('1. Number of partsNeeded:');
 
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
+let supplyChanges = [3, 5, -6, 0, 7, 11]
 console.log('2. Array of supplyChanges:');
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
-console.log('3. Second supplyChange is:');
+console.log('3. Second supplyChange is:', supplyChanges[1]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
+let removedItem = supplyChanges.pop();
 console.log('4. Removed item:');
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
+let newItem = supplyChanges.push(25);
 console.log('5. Adding 25 to supplyChanges.');
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
@@ -25,12 +29,22 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
-
+for (let x=0;  x< supplyChanges.length;  x++ ) {
+ if (supplyChanges[x] > 0) { console.log(`added ${supplyChanges[x]} parts.`);
+} else if (supplyChanges[x] < 0) { console.log (`removed ${supplyChanges[x]} parts`);
+} else { console.log('No change.', supplyChanges[x]);
+}
+}
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
+for (changedParts of supplyChanges){
+  if (changedParts<0) {console.log('Part count', changedParts);
+} else if (changedParts>0) console.log('Added', changedParts, 'parts');
+}
+
 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
