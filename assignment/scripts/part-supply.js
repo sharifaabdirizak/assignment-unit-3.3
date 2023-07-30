97e85e641c061510d68a18323f09fef2408c719e
Sharifa Abdirizak <sharifamabdirizak@gmail.com>
2023-07-30 18:33:34 -0500

    made corrections

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -4,24 +4,25 @@ console.log('****** Part Supply *******');
 // 1. Create a variable called 'partsNeeded' with a value of the number 40 
 //    & console.log the variable
 let partsNeeded = 40
-console.log('1. Number of partsNeeded:');
+console.log(partsNeeded)
 
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 let supplyChanges = [3, 5, -6, 0, 7, 11]
-console.log('2. Array of supplyChanges:');
+console.log(supplyChanges);
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
-console.log('3. Second supplyChange is:', supplyChanges[1]);
+console.log(supplyChanges[1]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
-let removedItem = supplyChanges.pop();
 console.log('4. Removed item:');
+console.log("remove last item", supplyChanges.pop())
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 let newItem = supplyChanges.push(25);
 console.log('5. Adding 25 to supplyChanges.');
+console.log(supplyChanges);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -49,6 +50,11 @@ for (changedParts of supplyChanges){
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
+let totalSupplies = 0;
+for(item of supplyChanges){
+    totalSupplies += item;
+}
+console.log(totalSupplies);
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
@@ -57,3 +63,10 @@ console.log('8. Total supplies available is:');
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+let filledBoxes = 0;
+let parts = 572;
+while(parts >= 7){
+    filledBoxes++;
+    parts -= 7;
+}
+console.log('The number of boxes filled was', filledBoxes, 'with', parts, 'parts left over.');
